b42867db2eb9d545e5d0ee966b00ec7fe6cf2d13
akshay shukla <shukla.akshay07@gmail.com>
2019-04-18 22:25:59 +0530

    mongo port changed

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const app = express();
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
-mongoose.connect("mongodb://localhost:3001/test");
+mongoose.connect("mongodb://localhost:27017/test");
 mongoose.connection.on('error', (err) => {
   console.error(err);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
